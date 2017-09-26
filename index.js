a3b7ebd936704f4ea5a79ae057297368805d7822
Jose Hernandez <josefhu15@gmail.com>
2017-09-25 18:29:38 -0700

    added html file to serve

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Hello! this is for my CECS 378 class!!! :)')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello! this is for my CECS 378 class!!! :)')
+// })
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
